13af2555cf400b69620385d8c0833dec0f841586
Chuck <tanowitz@gmail.com>
2016-04-03 21:10:04 -0400

    Assignment for Week 3 of R Programming
    This is the assignment for week 3 of the R programming class on
    Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions follow the direction of the course. The first caches a matrix.
+## The second returns a matrix that is the inverse of x.
 
-## Write a short comment describing this function
+## This makes a cache of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function()m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+
+## This solves for x, but checks first if the answer is cached.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
